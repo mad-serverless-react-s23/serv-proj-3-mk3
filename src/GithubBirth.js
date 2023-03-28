e3a77d962b1b6152cdde5e29ef8b4410cfddfcaf
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-03-28 16:34:15 -0500

    Update GithubBirth.js
    more info added

--- src/GithubBirth.js
+++ src/GithubBirth.js
@@ -18,10 +18,13 @@ export const GithubBirth = () => {
             <h1>{born.login} was forged on {born.created_at} and proceeded to alter the internet...</h1>
             <h2>More information about {born.login}:</h2>
             <ul>
+                <li>The alter-ego is {born.name}</li>
                 <li>Currently has {born.followers} followers</li>
                 <li>Is following {born.following} accounts</li>
                 <li>Has {born.public_repos} public repositories</li>
             </ul>
+            <p>About {born.login}: {born.bio}</p>
+            <p>Profile last updated {born.updated_at}</p>
         </>        
     )
 }
