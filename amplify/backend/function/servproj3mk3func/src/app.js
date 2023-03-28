88ce3f1ba2c71d5c66445a478b907b2138571979
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-03-28 14:54:26 -0500

    Update app.js
    comment before making next change

--- amplify/backend/function/servproj3mk3func/src/app.js
+++ amplify/backend/function/servproj3mk3func/src/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
 const axios = require('axios')
+// so far dummy data works without calling axios...
 
 // declare a new express app
 const app = express()
