4d8fa3f36c9ddf1ebc3602ce0ac4e9a65bfafc44
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-03-28 14:51:24 -0500

    Update App.js
    comment added just to mark functional api test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { Coins } from './Coins';
 import { GithubBirth } from './GithubBirth';
-// pushing axios to the function
+// pushed axios - so far the api still works
 function App() {
   return (
     <div className="App">
