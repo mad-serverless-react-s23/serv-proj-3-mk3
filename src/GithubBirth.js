d6a21659031de0cf8fec19871a21639fd071ce9b
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-03-28 14:26:29 -0500

    Update GithubBirth.js
    still not reading dummy data...

--- src/GithubBirth.js
+++ src/GithubBirth.js
@@ -17,8 +17,10 @@ export const GithubBirth = () => {
         <>
             <h2>GithubBirth works?</h2>
             {
-                born.map(x => (
-                    <h3>{x.name} was forged on {x.day} and proceeded to alter the internet...</h3>
+                born.map((x, index) => (
+                    <div key={index}>
+                        <h3>{x.name} was forged on {x.day} and proceeded to alter the internet...</h3>
+                    </div>
                 ))
             }
         </>        
