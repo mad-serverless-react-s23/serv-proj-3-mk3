7ed513374652ab1ee887fd005b461fe23318731a
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-03-28 13:11:31 -0500

    Update Coins.js
    built the data for it, next to add to App.js

--- src/Coins.js
+++ src/Coins.js
@@ -1,2 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import { API } from 'aws-amplify';
+
+export const Coins = () => {
+    const [coins, updateCoins] = useState([]);
+
+    const fetchCoins = async() => {
+        const data = await API.get('serveproj3mk3api', '/coins');
+        updateCoins(data.coins);
+    };
+
+    useEffect(() => {
+        fetchCoins();
+    }, []);
+
+    return (
+        {
+            coins.map((coin, index) => (
+                <div key={index}>
+                    <h2>{coin.name} - "{coin.symbol}"</h2>
+                    <h4>${coin.price_usd}</h4>
+                </div>
+            ))
+        }
+    )
+}
