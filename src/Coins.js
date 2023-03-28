c23c0f349a5325f996e3361e1ac7be75ff2e4d39
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-03-28 15:25:37 -0500

    added input fields
    probably need to push again...

--- src/Coins.js
+++ src/Coins.js
@@ -1,12 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import { API } from 'aws-amplify';
-// dummy data test successful
+
 export const Coins = () => {
     const [coins, updateCoins] = useState([]);
 
+    const [input, updateInput] = useState({ limit: 5, start: 0});
+
+    const updateInputValues = (type, value) => updateInput(
+        { ...input, [type]: value }
+    );
+
+    const [loading, updateLoading] = useState(true);
+
     const fetchCoins = async() => {
-        const data = await API.get('servproj3mk3api', '/coins');
+        updateLoading(true);
+    const { start, limit } = input;
+        const data = await API.get('servproj3mk3api', `/coins?start=${start}&limit=${limit}`);
         updateCoins(data.coins);
+        updateLoading(false);
     };
 
     useEffect(() => {
@@ -15,15 +26,30 @@ export const Coins = () => {
 
     return (
         <>
-        <h2>Coins works?</h2>
-        {
-            coins.map((coin, index) => (
-                <div key={index}>
-                    <h2>{coin.name} - "{coin.symbol}"</h2>
-                    <h4>${coin.price_usd}</h4>
-                </div>
-            ))
-        }
+            <h2>Coins works?</h2>
+            <input
+            onChange={e => updateInputValues('start', e.target.value)}
+            placeholder="start"
+            />
+            <hr/>
+            <input
+                onChange={e => updateInputValues('limit', e.target.value)}
+                placeholder="limit"
+            />
+            <hr/>
+            <button
+                onClick={fetchCoins}
+            >Fetch Coins</button>
+            <hr/>
+            {loading && <h2>Loading your request...</h2>}
+            {
+                !loading && coins.map((coin, index) => (
+                    <div key={index}>
+                        <h2>{coin.name} - "{coin.symbol}"</h2>
+                        <h4>${coin.price_usd}</h4>
+                    </div>
+                ))
+            }
         </>
         
     );
