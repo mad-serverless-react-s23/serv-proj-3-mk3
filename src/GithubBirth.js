db942a1fa4ef1931ec217fb69a894c64cdfda544
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-03-28 14:18:14 -0500

    Update GithubBirth.js
    not currently reading the birth info

--- src/GithubBirth.js
+++ src/GithubBirth.js
@@ -2,7 +2,25 @@ import React, { useState, useEffect } from 'react';
 import { API } from 'aws-amplify';
 
 export const GithubBirth = () => {
+    const [birth, updateBirth] = useState([]);
+
+    const fetchBirth = async() => {
+        const info = await API.get('servproj3mk3', '/born');
+        updateBirth(info.birth);
+    };
+
+    useEffect(() => {
+        fetchBirth();
+    }, []);
+
     return (
-        <h2>GithubBirth works?</h2>
+        <>
+            <h2>GithubBirth works?</h2>
+            {
+                birth.map((born) => (
+                    <h3>{born.name} was forged on {born.day} and proceeded to alter the internet...</h3>
+                ))
+            }
+        </>        
     )
 }
